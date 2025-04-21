5a84ddce9f3de345dc35b6fa60b1f1283c254226
Okana-401 <Issoufalynasry@gmail.com>
2025-04-21 11:04:26 +0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import ChatBox from "./components/ChatBox"
+
+function App() {
+  return (
+    <div className="bg-black min-h-screen text-white p-4">
+      <h1 className="text-3xl font-bold text-orange-500 mb-4">Tsara Hevitra</h1>
+      <ChatBox />
+    </div>
+  )
+}
+
+export default App
